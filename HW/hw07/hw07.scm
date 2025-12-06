caa7cffd2662d9679035258bc3ea65913425d606
lzt0911 <17863130590@163.com>
2025-12-06 09:49:49 +0800

    update hw07

---
+++ HW/hw07/hw07.scm
@@ -0,0 +1,18 @@
+(define (square n) (* n n))
+
+(define (pow base exp) 
+  (cond ((= exp 1) base)
+        ((even? exp) (square (pow base (quotient exp 2)))) 
+        (else (* base (square (pow base (quotient exp 2)))))))
+
+(define (repeatedly-cube n x)
+  (if (zero? n)
+      x
+      (let ((y (repeatedly-cube (- n 1) x)))
+        (* y y y))))
+
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) (car (cdr s)))
+
+(define (caddr s) (car (cdr (cdr s))))
